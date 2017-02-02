36e401e394dd30b09ae252737a8243ed8c7436e1
worstprogrammerCN <657278923@qq.com>
2017-02-02 22:46:45 +0800

    ..

---
+++ myapp/routes/assistants.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var debug = require('debug')('teacher');
+var Teacher = require('../userDefinition/teachers.js');
+
+
+function initializeDatabase(db){
+    Teacher.initializeDatabase(db);
+}
+router.use(function(req, res, next){
+  req.teacher = new Teacher(req.user);
+  next();
+})
+
+router.get('/index', function(req, res, next){
+
+});
+
+
+router.get('/profile', function(req, res, next){
+  
+});
+
+
+module.exports = {
+  router : router,
+  initializeDatabase : initializeDatabase
+};
