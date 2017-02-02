c0164fae4392049e81c7a002f9ca9ceec76e843b
worstprogrammerCN <657278923@qq.com>
2017-02-03 02:21:01 +0800

    ..

--- myapp/app.js
+++ myapp/app.js
@@ -39,7 +39,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//session and passpost
+
+
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
